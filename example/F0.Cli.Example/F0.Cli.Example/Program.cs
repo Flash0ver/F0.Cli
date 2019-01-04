f74679fc9f772b0f6c59c35409389f83b935bf25
Flash0ver <38893694+Flash0ver@users.noreply.github.com>
2019-01-04 06:36:32 +0100

    Add C# projects
    * production: .NET Standard 2.0
    * test: xUnit 2.4.1
    * sample: .NET Core 2.1

---
+++ example/F0.Cli.Example/F0.Cli.Example/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace F0.Cli.Example
+{
+	internal static class Program
+	{
+		private static void Main(string[] args)
+		{
+			Console.WriteLine("F0.Cli");
+			Console.WriteLine();
+		}
+	}
+}
